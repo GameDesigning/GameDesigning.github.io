5e8fe3c6b3b0c2274b712cdc309852c4b15ba469
Charlotte <charlotte.gayton@gmail.com>
2022-07-29 17:59:54 +0100

    resolved changes :/

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@
 import './App.css';
 import React, {useState} from 'react';
 
-// const multiplier = 0;
-// const chickenRate = 0;
-// const total = 0; 
-
-var start = Date.now()
-
 function App() {
   
   const [chickRate, setChickRate] = useState(0);
@@ -24,5 +18,3 @@ function App() {
 }
 
 export default App;
-
-// style={{backgroundImage: `url(${background})`}}
