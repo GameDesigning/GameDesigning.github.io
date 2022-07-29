4b72b41c006fc99ea26850a079550e4a1b0e0fbd
Charlotte <charlotte.gayton@gmail.com>
2022-07-29 18:06:54 +0100

    Add all the changes Danny made me stash

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@
 import './App.css';
 import React, {useState} from 'react';
 
+// var start = Date.now()
+
 function App() {
   
   const [chickRate, setChickRate] = useState(0);
+  const [totalMoney, setTotalMoney] = useState(0);
+
+  setInterval(function(){
+      setTotalMoney(chickRate + totalMoney);
+  }, 1000);
 
   return (
     <div className="App" >
       <header className="App-header">
         <h1>Farm Idle Game</h1>
         <button type="button" onClick={() => setChickRate(chickRate + 1)}>Click to increase chicken production rate</button>
-        <p>Chickens Making: {chickRate} per second</p>
+        <p>Chickens Making: {chickRate} per second <br></br>
+          Total Money {totalMoney}</p>
       </header>
     </div>
   );
